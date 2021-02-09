a505460d357a39d792db9594a59dd63e69785c83
benlebna ahmed <abenlebna@qualycloud.com>
2021-02-09 23:29:32 +0100

    ras

--- utilities/choixAleatoire.js
+++ utilities/choixAleatoire.js
@@ -7,10 +7,6 @@ const shuffle = o => {
 exports.rand = () => {
   let couleur = ['rouge', 'bleu', 'vert', 'jaune', 'violet', 'marron'];
   var rand = [];
-  // function shuffle(o) {
-  //   for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  //   return o;
-  // }
   for (var i = 0; i < 4; i++) {
     shuffle(couleur);
     rand.push(couleur.pop());
